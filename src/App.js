85db0824acfe60932b02c8889684fa54200af8e7
Minu Susan John <minususanj@gmail.com>
2025-11-09 16:15:08 +0530

    installed tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-red-300 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded transition-colors">
+      Hello Minus
     </div>
   );
 }
